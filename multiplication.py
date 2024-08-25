6e0c9c936123c6a4b8eefde75805cfdd263c757e
taher miyajee <taherthespymi7@gmail.com>
2024-08-25 12:55:14 +0530

    step 1

--- multiplication.py
+++ multiplication.py
@@ -1,1 +1,7 @@
-print("my name is taher")
+num=int(input("enter a number between 1-20"))
+if num>20: 
+    print("the number is gerater than 20 plz enter the number again")
+else:
+    for i in range(1,11):
+        print(num,'x',i,'=',num*i)
+
