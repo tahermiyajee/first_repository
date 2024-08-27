0f696f72e9e594bb49c26810df8ea46271e3d92f
taher miyajee <taherthespymi7@gmail.com>
2024-08-27 23:21:02 +0530

    program compleated

--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,11 @@
 num=int(input("enter a number between 1-20"))
-if num>20: 
-    print("the number is gerater than 20 plz enter the number again")
+if num>20 or num<1:
+    print("the number is gerater than 20 or less than 1 plz enter the number again")
 else:
-    for i in range(1,11):
-        print(num,'x',i,'=',num*i)
+     with open(f"multiplication_table_of_{num}.txt", "a") as f:
+          for i in range(1,11):
+               print(num,'x',i,'=',num*i, file=f)
+
+     
+ 
 
